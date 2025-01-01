ad2cc0101d2ba466cb527c15bbb8ffc1ae3e9b15
SyamGaraga <syamgaragaabc@gmail.com>
2025-01-01 11:14:09 +0530

    transforms

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,35 @@
-console.log("123");
+
+//  let ob1={
+//     name:"syam",
+//     id:"22"
+// }
+// console.log(ob1.id)
+
+// let str="welcome";
+// console.log(str[1])
+
+// var a=1;
+// var b=2;
+// console.log(a+"and"+b)
+
+// let obj1={
+//   id:"1",
+//   name:"syam"
+// }
+// let obj2=obj1;
+// obj2.name="abhi";
+// console.log(obj2,obj1);
+
+// a=1;
+// b=2;
+// console.log(`${a} and ${b}`)
+
+let ob={
+  name:"syam",
+  cash:500000,
+  from:"hyd",
+  to:"amp",
+  by:"car",
+  on:"monday"
+}
+console.log(`${ob.name} has travelled from ${ob.from} to ${ob.to} by ${ob.by} with cash of ${ob.cash} on ${ob.on}`)
