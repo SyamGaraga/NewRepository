6962ae2d808e8f7022a13e58854ba639915fd9ed
SyamGaraga <syamgaragaabc@gmail.com>
2025-02-05 23:21:50 +0530

    functions

---
+++ python/problem solving/fun_assignment.py
@@ -0,0 +1,18 @@
+#sum 
+def sum(a,b):
+    return a+b
+print(sum(2,3))
+
+#min to second
+def con(a):
+    return a*60
+print(con(2))
+
+
+def str(a,b):
+    if a+b==10 or (a==10 or b==10):
+        return True
+    else:
+        return False
+print(str(1,1))
+
